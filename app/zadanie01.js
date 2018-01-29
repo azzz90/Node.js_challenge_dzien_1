2766b92e5c6b8d1de4b7d73a5583fc5f5c9c6fbf
azzz90 <aleksander.ziolkowski90@gmail.com>
2018-01-29 22:39:16 +0100

    pierwsze 3 zadania na szybko

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,6 @@
 //Twój kod
+console.log('Olek Zzzzz');
+
+setTimeout(()=> {
+  console.log('wita się z Node.js!');
+},4000);
--- app/zadanie03.js
+++ app/zadanie03.js
@@ -1,1 +1,14 @@
 //Twój kod
+let myNumbers = process.argv.slice(2);
+
+//console.log(myNumbers);
+
+let myNumbersFixed = myNumbers.map(arg => {
+  return parseInt(arg);
+})
+
+//console.log(myNumbersFixed);
+
+let result = myNumbersFixed.reduce((a,b) => a+b,0);
+
+console.log(result);
